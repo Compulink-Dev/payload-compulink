9a9d4ecc10b9230c97b5da9ad796620fbd35ba61
Compulink-Dev <devcompulink@gmail.com>
2025-11-21 12:57:20 +0200

    blob-reverse

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,10 +1,10 @@
+// storage-adapter-import-placeholder
 import { mongooseAdapter } from '@payloadcms/db-mongodb'
 import { lexicalEditor } from '@payloadcms/richtext-lexical'
 import path from 'path'
 import { buildConfig } from 'payload'
 import { fileURLToPath } from 'url'
 import sharp from 'sharp'
-import { vercelBlobStorage } from '@payloadcms/storage-vercel-blob'
 
 import { Users } from './collections/Users'
 import { Media } from './collections/Media'
@@ -34,11 +34,6 @@ export default buildConfig({
   }),
   sharp,
   plugins: [
-    vercelBlobStorage({
-      collections: {
-        media: true, // Only this format is supported
-      },
-      token: process.env.BLOB_READ_WRITE_TOKEN,
-    }),
+    // storage-adapter-placeholder
   ],
 })
